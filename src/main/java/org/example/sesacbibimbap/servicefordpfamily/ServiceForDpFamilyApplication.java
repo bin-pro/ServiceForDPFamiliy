5717e8312ec682799728ad14a58d1b2dfaec5a41
Subin <70842920+bin-pro@users.noreply.github.com>
2024-07-16 17:25:29 +0900

    Build: init service

---
+++ src/main/java/org/example/sesacbibimbap/servicefordpfamily/ServiceForDpFamilyApplication.java
@@ -0,0 +1,13 @@
+package org.example.sesacbibimbap.servicefordpfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceForDpFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceForDpFamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sesacbibimbap/servicefordpfamily/ServiceForDpFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sesacbibimbap.servicefordpfamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceForDpFamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
